1ee9048ce2cf5eca21bc3a88c63552808f188691
ojassharma <ojasvisharmacs@gmail.com>
2025-06-04 19:09:52 +0530

    color change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black flex flex-col items-center justify-center text-white text-center p-4 gap-1">
+    <div className="min-h-screen bg-[#1e8da9] flex flex-col items-center justify-center text-white text-center p-4 gap-1">
       <h1 className="text-4xl sm:text-6xl font-bold mb-2">Intellect-Nest 🧠</h1>
       <p className="text-xs sm:text-sm opacity-70">
         you have reached at the start of this project timeline
