9a9e1a9125ed5a8d0540a86649e6bb3d34a24f91
ojassharma <ojasvisharmacs@gmail.com>
2025-06-11 21:37:58 +0530

    3d scene

---
+++ src/components/ChessModel.js
@@ -0,0 +1,9 @@
+"use client";
+import { useGLTF } from "@react-three/drei";
+
+export function ChessModel(props) {
+  const { scene } = useGLTF("/models/chess.glb");
+  return <primitive object={scene} {...props} />;
+}
+
+useGLTF.preload("/models/chess.glb");
